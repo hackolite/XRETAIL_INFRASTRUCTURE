6ce7c9796adfc59d574571111dfa123f84684b29
hackolite <laureote-loic@hotmail.fr>
2023-11-17 18:50:03 +0100

    add algo

---
+++ regbbox/data_management/preprocessing.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+VOC2012 preprocessing to speed up training
+"""
+import os
+import sys
+from glob import glob
+
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+
+from models.SSD300 import SSD300
+
+from .VOC2012ManagerObjDetection import VOC2012ManagerObjDetection
+
+sys.path.insert(1, '../')
+
+
+def saveGTdata(voc2012path, output_path):
+    """
+    Method to save data in a proper format to train the network
+
+    Args:
+        - (str) path to save data
+    """
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    db_manager = VOC2012ManagerObjDetection(voc2012path + '/', batch_size=32, floatType=32)
+    SSD300_model = SSD300(21, floatType=32)
+    for i, batch in enumerate(tqdm(db_manager.batches)):
+        # get data from batch
+        imgs, confs, locs = db_manager.getImagesAndGtSpeedUp(batch, SSD300_model.default_boxes)
+        np.save(output_path + "/imgs_{:05d}.npy".format(i), imgs, allow_pickle=True)
+        np.save(output_path + "/confs_{:05d}.npy".format(i), confs, allow_pickle=True)
+        np.save(output_path + "locs_{:05d}.npy".format(i), locs, allow_pickle=True)
+
+
+def loadGTdata(path, nb_data_to_load=-1):
+    """
+    Method to load needed data for training
+    N: batch size
+    B: batch size
+    O: number of objects in a given image
+    D: number of default boxes
+
+    Args:
+        - (str) path to load
+
+    Return:
+        - (list of tf.Tensor) images (B, 300, 300, 3)
+        - (list of tf.Tensor) confs gt (N, B, O, D)
+        - (list of tf.Tensor) locs gt (N, B, O, D, 4)
+    """
+    imgs = []
+    for batch in tqdm(sorted(glob(path + "/imgs*.npy"))[:nb_data_to_load]):
+        # get data from batch
+        imgs.append(tf.convert_to_tensor(np.load(batch, allow_pickle=True)))
+
+    confs = []
+    for batch in tqdm(sorted(glob(path + "/confs*.npy"))[:nb_data_to_load]):
+        # get data from batch
+        confs.append(tf.convert_to_tensor(np.load(batch, allow_pickle=True)))
+
+    locs = []
+    for batch in tqdm(sorted(glob(path + "/locs*.npy"))[:nb_data_to_load]):
+        # get data from batch
+        locs.append(tf.convert_to_tensor(np.load(batch, allow_pickle=True)))
+
+    return imgs, confs, locs
+
+
+def loadSpecificGTdata(path, idx):
+    """
+    Method to load a particular batch
+    B: batch size
+    N: number of objects in a given image
+    D: number of default boxes
+
+    Args:
+        - (str) path to load
+
+    Return:
+        - (list of tf.Tensor) images (B, 300, 300, 3)
+        - (list of tf.Tensor) confs gt (B, N, D)
+        - (list of tf.Tensor) locs gt (B, N, D, 4)
+    """
+    batch = sorted(glob(path + "/imgs*.npy"))[idx]
+    imgs = tf.convert_to_tensor(np.load(batch, allow_pickle=True))
+
+    batch = sorted(glob(path + "/confs*.npy"))[idx]
+    confs = tf.convert_to_tensor(np.load(batch, allow_pickle=True))
+
+    batch = sorted(glob(path + "/locs*.npy"))[idx]
+    locs = tf.convert_to_tensor(np.load(batch, allow_pickle=True))
+
+    return imgs, confs, locs
---
+++ regbbox/utils/train.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Function to train SSD
+"""
+
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+
+
+def train(model, optimizer, imgs, confs, locs, weights_path, num_epoch=250, inter_save=5):
+    """
+    Method to train SSD architecture
+
+    Args:
+        - SSD300 class from models/SSD300.py
+        - (tf.keras.optimizers) optimizer to use
+        - VOC2012ManagerObjDetection class from data/
+        - (str) path to save weights
+        - (list) batches of images name
+        - (int) number of epochs
+        - (int) interval to save weights
+    """
+    for epoch in range(num_epoch):
+        losses = []
+        for i in tqdm(range(len(imgs))):
+            # get data from batch
+            images, confs_gt, locs_gt = imgs[i], confs[i], locs[i]
+            # get predictions and losses
+            with tf.GradientTape() as tape:
+                confs_pred, locs_pred = model(images)
+                # concat tensor to delete the dimension of feature maps
+                confs_pred = tf.concat(confs_pred, axis=1)
+                locs_pred = tf.concat(locs_pred, axis=1)
+
+                # calculate loss
+                confs_loss, locs_loss = model.calculateLoss(
+                    confs_pred, confs_gt, locs_pred, locs_gt
+                )
+                loss = confs_loss + 1 * locs_loss  # alpha equals 1
+                l2 = [tf.nn.l2_loss(t).numpy() for t in model.trainable_variables]
+                loss = loss + 0.001 * tf.math.reduce_sum(l2)
+                losses.append(loss)
+
+            # back propagation
+            gradients = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        print("Mean loss: {} on epoch {}".format(np.mean(losses), epoch))
+        if epoch % inter_save == 0:
+            model.save_weights(weights_path + "/ssd_weights_epoch_{:03d}.h5".format(epoch))
